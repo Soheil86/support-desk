ee0fd457cf709b8979beb708f5f374d9d58c565e
Soheil Ghanbari <soheilghanbari1365@gmail.com>
2022-05-03 15:49:06 +0200

    Add Tickets to Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,9 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
+import NewTicket from './pages/NewTicket'
 import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            {/* <Route path='/new-ticket' element={<PrivateRoute />}> */}
+            <Route path='/new-ticket' element={<NewTicket />} />
+            {/* </Route> */}
           </Routes>
         </div>
       </Router>
