f91a52fd5f162e1c0c48e210e1eeb599288ab848
Soheil Ghanbari <soheilghanbari1365@gmail.com>
2022-05-19 16:31:34 +0200

    production change

--- backend/server.js
+++ backend/server.js
@@ -6,42 +6,30 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+// Connect to database
 connectDB()
 
 const app = express()
 
-const cors = require('cors')
-const corsOptions = {
-  origin: '*',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
-
-app.use(cors(corsOptions))
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk Application ' })
-})
-
-//Routes
+// Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-//Serve Frontend
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set build folder as Static
+  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  )
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
   app.get('/', (req, res) => {
-    res
-      .status(200)
-      .json({ message: 'Welcomeeeeeeee to the Support Desk Application ' })
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
 }
 
